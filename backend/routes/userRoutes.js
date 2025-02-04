ade3a163ca24e6c4a8bf7e536827c3981db6b06b
Gabriel Teo <gtforce212@gmail.com>
2025-02-04 09:51:46 +0800

    BE: Remove old logic

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userController = require('../controllers/userController');
-
-router.get('/', userController.getUsers);
-router.get('/create', userController.createUser);
-router.get('/update', userController.updateUser);
-
-module.exports = router;
