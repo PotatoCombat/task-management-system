996f32e834c64ed50e8486a4d2eb244ad04abb33
Gabriel Teo <gtforce212@gmail.com>
2025-02-10 06:46:59 +0800

    FE: Add table components

---
+++ frontend/src/components/table/SwitchCell.jsx
@@ -0,0 +1,28 @@
+import { Switch, TableCell } from '@mui/material';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+
+const SwitchCell = forwardRef(({
+  defaultChecked,
+  onChange,
+  ...props
+}, ref) => {
+  const [value, setValue] = useState(defaultChecked ? 1 : 0);
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => value,
+    setValue: setValue,
+  }));
+
+  const handleChange = (event) => {
+    setValue(event.target.checked);
+    onChange?.(event.target.checked);
+  };
+
+  return (
+    <TableCell className='table-cell'>
+      <Switch color='primary' onChange={handleChange} {...props} checked={value} />
+    </TableCell>
+  );
+});
+
+export default SwitchCell;
