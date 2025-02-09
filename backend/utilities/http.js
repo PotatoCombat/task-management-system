389d879c6e105e3a100e82b41d33c203cafeee50
Gabriel Teo <gtforce212@gmail.com>
2025-02-09 23:18:20 +0800

    BE: Add http error handler

---
+++ backend/utilities/http.js
@@ -0,0 +1,10 @@
+// Always pass errors to error handler
+function asyncHandler(fn) {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+module.exports = {
+  asyncHandler
+};
