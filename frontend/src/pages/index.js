bfc7c4c308ea04de565aa5eb9810bf087a9a52ff
Gabriel Teo <gtforce212@gmail.com>
2025-02-10 06:51:02 +0800

    FE: Add error page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as ErrorPage } from './ErrorPage';
 export { default as LoginPage } from './LoginPage';
 export { default as LogoutPage } from './LogoutPage';
 export { default as TasksPage } from './TasksPage';
